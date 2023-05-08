d80deeae1b79f8050606b0015068017b9f11bf1b
AgustinVN <agusnarvi@hotmail.com>
2023-05-08 02:49:32 -0300

    se corrijen errores de la primera entrega

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
-var cantidad,precio,descuento,compra,pagar;
-
-precio = parseInt(prompt("Ingrese el precio"))
-cantidad = parseInt(prompt("Ingrese la cantidad"))
-
-compra = precio*cantidad;
-desc =compra*0.25;
-pagar = compra-desc;
-
-alert("Total a pagar " + compra)
-alert("Monto final con el 25% de descuento: " + pagar)
+let nombreUsuario = prompt("Ingrese su nombre")
+let edad = prompt(nombreUsuario + " Cuantos años tienes?");
+    if (edad >=18) {
+        alert ("Bienvenido");
+    } else {
+        alert ("Lo siento " + nombreUsuario + " no cumples los requisitos necesarios para ingresar");
+    }
+let descuentos = prompt("¿Desea recibir notificaciones a su correo de futuros descuentos?")
+        if(descuentos === "si") {
+let ingresarCorreo = prompt("Genial! porfavor ingrese su correo para ser el primero en recibir futuras ofertas");
+    }
+let compra = prompt("¿Que desea comprar? camisa por 10$, pantalon por 15$ o zapatillas por 25$?");
+ if(compra === "camisa") {
+    while (true) {
+      let respuesta = prompt("¿Desea comprar este artículo?");
+      if(respuesta === "si") {
+        alert("¡Excelente! Gracias por su compra :D");
+        break;
+      }
+    }
+  } else if(compra === "pantalon") {
+    while (true) {
+        let respuesta = prompt("¿Desea comprar este artículo?");
+        if(respuesta === "si") {
+        alert("¡Excelente! Gracias por su compra :D");
+        break;
+      }
+    }
+  } else if(compra === "zapatillas")
+    while (true) {
+        let respuesta = prompt("¿Desea comprar este artículo?");
+        if(respuesta === "si") {
+          alert("¡Excelente! Gracias por su compra :D");
+          break;
+    }
+  }
